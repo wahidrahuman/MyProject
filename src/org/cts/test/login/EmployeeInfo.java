21fbfd61492ef53f40725c654830944268a68b36
wahidrahuman <wahidrahuman1996@gmail.com>
2019-12-06 12:46:04 +0530

    Task

---
+++ src/org/cts/test/login/EmployeeInfo.java
@@ -0,0 +1,30 @@
+package org.cts.test.login;
+
+public class EmployeeInfo {
+public static void main(String[] args) {
+	String name="kavin";
+	System.out.println("empname"+name);
+	byte age=25;
+	System.out.println(age);
+	short bikeNo=3365;
+	System.out.println(bikeNo);
+	int i=56565656;
+	System.out.println(i);
+	long phNo=4545464842l;
+	System.out.println(phNo);
+	float salary=10/7f;
+	System.out.println(salary);
+	double deposit=3/2;
+	System.out.println(deposit);
+	double deposit1=10/7d;
+	System.out.println(deposit1);
+	char initial='c';
+	System.out.println(initial);
+	
+	
+	
+	
+	
+	
+}
+}
